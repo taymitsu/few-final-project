918b053afd3412ba9e06a8bb469d05d30b97846e
taymitsu <tmitsumori96@gmail.com>
2023-03-01 14:42:28 -0800

    API fetching correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Recipe from './Recipe.js'
 
 function App() {
   return (
     <div className="App">
       What's For Dinner?
+
+      <Recipe />
     </div>
+    
   );
 }
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import { useState } from 'react';
 import './Recipe.css'
 
 function Recipe() {
+  const[ingredients, setIngredients] = useState(' ')
+  const [recipes, setRecipes] = useState([])
+  const[data, setData] = useState(null)
+  
 
+  async function fetchRecipe() {
+    const apikey = "1018f2a014dc4a8d9e8ccf5656e9dea3"
+    const path = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apikey}&query=${ingredients}`
+    const res = await fetch(path)
+    const data = await res.json()
 
-  //async function fetchRecipe() {
-    //const apikey = "1018f2a014dc4a8d9e8ccf5656e9dea3"
-    //const path = "https://api.spoonacular.com/recipes/complexSearch"
+    console.log(data);
+    
+    setRecipes(data.results)
+
+    setData({
+      ingredients,
+      recipes
+    })
   }
+
+  return(
+    <div className="Ingredients">
+      <form onSubmit={e => {
+        e.preventDefault()
+        fetchRecipe()
+      }}>
+        <div>
+          <input
+            placeholder="Search Recipes by ingredient"
+            value={ingredients}
+            onChange={e => setIngredients(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+          </div>
+        ))}
+      </div>
+
+    </div>
+  )
 }
+
+export default Recipe;
