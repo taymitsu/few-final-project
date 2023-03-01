495ced05f5b21696e3534f4d861addca7941373d
taymitsu <tmitsumori96@gmail.com>
2023-03-01 13:39:16 -0800

    redux installed

---
+++ src/Recipe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Recipe.css'
+
+function Recipe() {
+
+
+  //async function fetchRecipe() {
+    //const apikey = "1018f2a014dc4a8d9e8ccf5656e9dea3"
+    //const path = "https://api.spoonacular.com/recipes/complexSearch"
+  }
+}
